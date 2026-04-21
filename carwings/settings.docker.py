69f7968248e3df970d1e68568e003e1db3ced6b9
Gaultier HUBERT <gaultier.hubert@gmail.com>
2026-04-21 10:28:38 +0200

    fix(deploy): serve static and media assets correctly in production
    Use absolute STATIC_URL/MEDIA_URL values and configure nginx aliases for /static and /data so pages do not generate broken relative asset paths that lead to 404s.

--- carwings/settings.docker.py
+++ carwings/settings.docker.py
@@ -152,11 +152,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
-MEDIA_URL = 'data/'
+MEDIA_URL = '/data/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
