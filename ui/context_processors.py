f7deb9c334793d3a3a9b0725d49176c6a3ecfce4
Gaultier HUBERT <gaultier.hubert@gmail.com>
2026-04-22 17:19:14 +0200

    fix

--- ui/context_processors.py
+++ ui/context_processors.py
@@ -1,11 +1,16 @@
 import os
 
 from git import Repo
+from git.exc import InvalidGitRepositoryError, NoSuchPathError
 
 from tculink import VERSION
 
-repo = Repo(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-commit_hash = repo.git.rev_parse("HEAD")
+commit_hash = os.environ.get("OPENCARWINGS_COMMIT_SHA", "unknown")
+try:
+    repo = Repo(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    commit_hash = repo.git.rev_parse("HEAD")
+except (InvalidGitRepositoryError, NoSuchPathError, ValueError):
+    pass
 
 
 def load_ver_info(request):
